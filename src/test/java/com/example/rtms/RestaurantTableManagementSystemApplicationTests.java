bb449d781d88bba3cab144b7e4806200fccc0075
Kshitij Bajracharya <bkshitij.dev@gmail.com>
2024-08-30 21:37:42 +0545

    Add initial files

---
+++ src/test/java/com/example/rtms/RestaurantTableManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rtms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantTableManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
